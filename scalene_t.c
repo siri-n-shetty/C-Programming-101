5590ba249794cfd5a7e0e92b77d4b1913913a91f
Siri N Shetty <114021638+siri-n-shetty@users.noreply.github.com>
2023-03-19 11:54:48 +0530

    area of a scalene triangle

---
+++ scalene_t.c
@@ -0,0 +1,25 @@
+// Finding the area of a scalene triangle
+// Scalene triangle does not have equal sides throughout
+// Area of triangle with two sides and angle included between them
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    float a, b, angle, area, rad;
+    float PI = 3.14;
+
+    printf("Enter length of side a: ");
+    scanf("%f", &a);
+
+    printf("Enter length of side b: ");
+    scanf("%f", &b);
+
+    printf("Enter angle between them: ");
+    scanf("%f", &angle);
+
+    rad = (PI/180)*angle;
+    area = (a*b*sin(rad))/2;
+
+    printf("The area of the triangle is: %.3f", area);
+}
